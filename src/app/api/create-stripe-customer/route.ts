34234203e7da03f0a7beee37bffa5af81314084d
*dJdCucEGeo <msipod3394@gmail.com>
2024-08-02 15:11:35 +0900

    supabaseにstripe顧客idを追加

--- src/app/api/create-stripe-customer/route.ts
+++ src/app/api/create-stripe-customer/route.ts
@@ -1,23 +1,55 @@
+import { AuthOptions } from "@/lib/auth/AuthOptions";
+import { db } from "@/lib/db";
+import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
 import Stripe from "stripe";
 
-// stripeに顧客データを作成
 export async function POST(req: NextRequest) {
-  const data = await req.json();
-  const { email } = data;
+  try {
+    /**
+     * stripeに顧客データを作成
+     */
+    const data = await req.json();
+    const { email } = data;
 
-  console.log(email);
+    // stripe初期化
+    const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
 
-  // stripe初期化
-  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
+    // 顧客データ作成
+    const customer = await stripe.customers.create({ email });
 
-  // 顧客データ追加
-  const customer = await stripe.customers.create({
-    email,
-  });
-  console.log(customer);
+    /**
+     * supabaseにstripe顧客idを追加
+     */
+    // セッション取得
+    const session = await getServerSession(AuthOptions);
 
-  return NextResponse.json({
-    message: `stripeに顧客情報が追加されました ${customer.id}`,
-  });
+    // セッションがなければ、403エラーを返す
+    if (!session) {
+      return NextResponse.json("アクセス権限がありません", { status: 403 });
+    }
+
+    // セッションからuserを取り出す
+    const { user } = session;
+
+    // supabaseにstripe顧客idを追加
+    const supabaseUser = await db.user.update({
+      where: { id: user.id },
+      data: { stripeCustomerId: customer.id },
+    });
+    console.log(supabaseUser);
+
+    return NextResponse.json({
+      message: `stripeに顧客情報が追加されました ${customer.id}`,
+    });
+  } catch (error) {
+    console.error("error", error);
+
+    return NextResponse.json(
+      {
+        error: "サーバーエラー",
+      },
+      { status: 500 }
+    );
+  }
 }
